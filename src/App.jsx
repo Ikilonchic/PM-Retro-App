fe14b7213275a8a77b9ebed87041cb0385202632
Ikilonchic <43388441+Ikilonchic@users.noreply.github.com>
2021-03-04 18:59:31 +0200

    Configure project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import logo from './logo.svg';
+import './App.css';
+import Retrospective from './components/Retrospective/Retrospective';
+
+const App = () => <Retrospective/>;
+
+export default App;
